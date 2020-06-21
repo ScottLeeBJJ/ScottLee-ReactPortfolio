6e73aa2772843c9807eb3dcaaea2040579dc7dc5
ScottLeeBJJ <scottlee5455@outlook.com>
2020-06-21 12:43:37 -0700

    updated blog

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,11 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+
 import axios from 'axios';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons";
 
 import PortfolioContainer from './portfolio/portfolio-container'
 import NavigationContainer from './navigation/navigation-container'
@@ -12,10 +16,13 @@ import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Blog from './pages/blog';
+import BlogDetail from './pages/blog-detail';
 import PortfolioManager from './pages/portfolio-manager';
 import PortfolioDetail from './portfolio/portfolio-detail';
 import Auth from './pages/auth';
 import NoMatch from './pages/no-match';
+
+library.add(faTrash, faSignOutAlt, faEdit);
 
 export default class App extends Component {
   constructor(props) {
@@ -125,6 +132,8 @@ export default class App extends Component {
             <Route  path="/contact" component={Contact} />
 
             <Route  path="/blog" component={Blog} />
+            
+            <Route  path="/b/:slug" component={BlogDetail} />
 
                 {this.state.loggedInStatus === "LOGGED_IN" ?  this.authorizedPages() : null}
 
